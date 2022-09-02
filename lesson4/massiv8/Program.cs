c02d02f677309ffede7eda7f299a017a09c6127f
Alexander <alex-lama@yandex.ru>
2022-09-02 17:43:40 +0300

    massiv8

---
+++ lesson4/massiv8/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] n = new int[8];
+for (int i = 0; i < 8; i++)
+    {
+    n[i] = new Random().Next(1,50);
+    }
+void data (int a, int b)
+{
+    for (int i = a; i<=b; i++)  Console.Write($"{n[i]}, ");
+}   
+data (0,4);
+Console.WriteLine();
+data (5,7);   
+Console.WriteLine();
