19615d6e4f06c29cf02aeaa5c417e0117fee805f
Alexander <alex-lama@yandex.ru>
2022-09-02 15:48:31 +0300

    сумма цифр числа

---
+++ lesson4/numSum/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    Console.WriteLine("Введите число");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int numSum = 0;
+    while (n>0)
+    {
+    numSum = n%10 + numSum;
+    n = n/10;
+    }
+    Console.WriteLine(numSum);
+}
+catch 
+    {
+    Console.WriteLine ("Допустимо вводить только цифры");
+    }
