3f542dedd447d3ac92db268819d6d1ed4210bcbd
Alexander <alex-lama@yandex.ru>
2022-09-12 15:41:05 +0300

    Ну и накрутил!!!

---
+++ task41/Program.cs
@@ -0,0 +1,18 @@
+﻿int n=0;
+void KeyAsk()
+{var x=Console.ReadLine();
+
+if (x!=$"") 
+{   
+    int z=Convert.ToInt32(x);
+    if (z>0)  
+   {
+        n=n+1;
+      KeyAsk();
+   }
+   else KeyAsk();
+}    
+else Console.WriteLine(n);
+}
+Console.WriteLine("Введите число или нажмите ввод");
+KeyAsk();
