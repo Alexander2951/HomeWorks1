3b41f0ff142b9ddb92e041bc57235c1cc476f083
Alexander <alex-lama@yandex.ru>
2022-09-18 13:48:46 +0300

    Пункт первый - уточнить задание. Пункт второй - не делать лишнего. В задании сказано неповторяющихся чисел, не сказано что они должны быть случайными

---
+++ task60/Program.cs
@@ -0,0 +1,34 @@
+﻿try
+{
+    Console.WriteLine("Задайте количество строк");
+    int str = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Задайте количество колонок");
+    int column = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Задайте  глубину");
+    int layer = Convert.ToInt32(Console.ReadLine());
+    void Box(int x, int y, int z)
+    {
+        int[,,] box = new int[x, y, z];
+        int num = 10;
+        for (int i = 0; i < z; i++)
+        {
+            for (int n = 0; n < y; n++)
+            {
+                for (int m = 0; m < x; m++)
+                {
+                    box[m, n, i] = num;
+                    num++;
+                    Console.Write($"{box[m, n, i]} ({m},{n},{i}) \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+    Box(str, column, layer);
+}
+catch
+{
+    Console.WriteLine("Только числовые значения");
+    throw;
+}
