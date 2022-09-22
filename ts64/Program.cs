210663e6b48a1b7b411b3f4611b97722cb1bb4f1
Alexander <alex-lama@yandex.ru>
2022-09-22 22:10:48 +0300

    PrintInterval

---
+++ ts64/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintInterval(int arg1, int arg2)
+{
+    if (arg1 == arg2)
+    {
+        Console.WriteLine(arg1);
+        return;
+    }
+    if (arg1 > arg2)
+    {
+        int temp = arg2; arg2 = arg1; arg1 = temp;
+    }
+    Console.Write(arg1 + ", "); arg1 = arg1 + 1; PrintInterval(arg1, arg2);
+}
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintInterval(m,n);
